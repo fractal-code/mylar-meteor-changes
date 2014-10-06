7c1a5f9f3e4b0a69c51ee87cf348439e0bae4f74
Yaser Jarrar <yaser@grout.io>
2014-10-06 19:10:04 +0400

    Replace store 'update' method to call intercept_*

--- livedata_connection.js
+++ livedata_connection.js
@@ -1,4 +1,6 @@
 //replace _process_ready in Connection prototype to call intercept.on_ready
+/* replace store 'update' method to add calls to intercept_* by modifying it from within
+   the wrappedStore object passed into registerStore in Connection prototype */
 
 //hack - create connection to dummy url to access Connection prototype
 var connection = DDP.connect('dummy_url');
@@ -32,6 +34,99 @@ connection.__proto__._process_ready = function (msg, updates) {
             }
         });
     });
+};
+
+//replace registerStore in Connection prototype
+connection.__proto__.registerStore = function (name, wrappedStore) {
+    var self = this;
+
+    if (name in self._stores)
+        return false;
+
+    //replace store 'update' method' to add calls to intercept_*
+    wrappedStore['update'] = function (msg) {
+        var mongoId = LocalCollection._idParse(msg.id);
+        var doc = self._collection.findOne(mongoId);
+
+        // Is this a "replace the whole doc" message coming from the quiescence
+        // of method writes to an object? (Note that 'undefined' is a valid
+        // value meaning "remove it".)
+        if (msg.msg === 'replace') {
+            var replace = msg.replace;
+            if (!replace) {
+                if (doc)
+                    self._collection.remove(mongoId);
+            } else {
+                intercept_in(self, mongoId, replace, function() {
+                    if (!doc) {
+                        self._collection.insert(replace);
+                    } else {
+                        // XXX check that replace has no $ ops
+                        self._collection.update(mongoId, replace);
+                    }
+                });
+            }
+        } else if (msg.msg === 'added') {
+            intercept_in(self, mongoId, msg.fields, function() {
+                if (doc) {
+                    throw new Error("Expected not to find a document already present for an add");
+                }
+                self._collection.insert(_.extend({_id: mongoId}, msg.fields));
+            });
+        } else if (msg.msg === 'removed') {
+            if (!doc)
+                throw new Error("Expected to find a document already present for removed");
+            self._collection.remove(mongoId);
+        } else if (msg.msg === 'changed') {
+            if (!doc)
+                throw new Error("Expected to find a document to change");
+            if (!_.isEmpty(msg.fields)) {
+                var modifier = {};
+                _.each(msg.fields, function (value, key) {
+                    if (value === undefined) {
+                        if (!modifier.$unset)
+                            modifier.$unset = {};
+                        modifier.$unset[key] = 1;
+                    } else {
+                        if (!modifier.$set)
+                            modifier.$set = {};
+                        modifier.$set[key] = value;
+                    }
+                });
+                intercept_in(self, mongoId, modifier.$set, function() {
+                    self._collection.update(mongoId, modifier);
+                });
+            }
+        } else {
+            throw new Error("I don't know how to deal with this message");
+        }
+    };
+
+    // Wrap the input object in an object which makes any store method not
+    // implemented by 'store' into a no-op.
+    var store = {};
+    _.each(['update', 'beginUpdate', 'endUpdate', 'saveOriginals',
+        'retrieveOriginals'], function (method) {
+        store[method] = function () {
+            return (wrappedStore[method]
+                ? wrappedStore[method].apply(wrappedStore, arguments)
+                : undefined);
+        };
+    });
+
+    self._stores[name] = store;
+
+    var queued = self._updatesForUnknownStores[name];
+    if (queued) {
+        store.beginUpdate(queued.length, false);
+        _.each(queued, function (msg) {
+            store.update(msg);
+        });
+        store.endUpdate();
+        delete self._updatesForUnknownStores[name];
+    }
+
+    return true;
 };
 
 //disconnect from dummy_url
