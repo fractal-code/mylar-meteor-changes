4ba374dc90b153725479e7b209c352817b576aff
Yaser Jarrar <yaser@grout.io>
2014-10-06 09:04:40 +0400

    Add package description & empty definition

---
+++ package.js
@@ -0,0 +1,10 @@
+Package.describe({
+    summary: 'Mylar, meteor changes',
+    version: '0.0.0',
+    name: 'mylar:meteor-changes',
+    git: 'https://github.com/gliesesoftware/mylar-meteor-changes.git'
+});
+
+Package.onUse(function (api) {
+    //add files and export
+});
