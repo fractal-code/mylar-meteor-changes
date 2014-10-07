5b4fe0bf917b7143b2bc7b3a0093b262ccace189
Yaser Jarrar <yaser@grout.io>
2014-10-07 17:42:19 +0400

    Remove redundant checks in intercept_* functions

--- helpers.js
+++ helpers.js
@@ -2,14 +2,14 @@
 
 // intercept_* functions
 intercept_out = function (collection, container, callback) {
-    if (Meteor.isClient && Mongo.Collection.intercept && Mongo.Collection.intercept.out) {
+    if (Meteor.isClient) {
         Mongo.Collection.intercept.out(collection, container, callback);
     } else {
         callback && callback();
     }
 };
 intercept_in = function (collection, id, container, callback) {
-    if (Meteor.isClient && Mongo.Collection.intercept && Mongo.Collection.intercept.incoming) {
+    if (Meteor.isClient) {
         Mongo.Collection.intercept.incoming(collection, id, container, callback);
     } else {
         callback && callback();
