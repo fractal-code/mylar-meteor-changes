60f84d2f9eac543fb9ceefb3fad3d64163e58b13
Yaser Jarrar <yaser@grout.io>
2014-10-08 16:55:48 +0400

    Bump version to 0.0.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: 'Mylar, meteor changes',
-    version: '0.0.0',
+    version: '0.0.1',
     name: 'mylar:meteor-changes',
     git: 'https://github.com/gliesesoftware/mylar-meteor-changes.git'
 });
